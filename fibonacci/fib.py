43ef3db11f8fee645f1e0d13dc491fbf8ca27afb
Andrew Chavez <chavandrew@gmail.com>
2017-11-14 07:22:55 -0600

    homework

--- fibonacci/fib.py
+++ fibonacci/fib.py
@@ -1,2 +1,4 @@
+import sys
+
 def fib(x):
   # Your code here
---
+++ fizzbuzz/fizzbuzz.py
@@ -0,0 +1,15 @@
+import sys
+
+def fizzbuzz(max_num):
+
+  for num in range(1, max_num):
+    if num % 3 == 0 and num % 5 == 0:
+      print('FizzBuzz')
+    elif num >= 1 and <= 100
+      print('Fizz')
+    elif num % 3 == 0:
+      print('Buzz')
+    else:
+      print(num)
+
+fizzbuzz(10)
